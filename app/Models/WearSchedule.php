86053ca914a3179b481a3d3b117a44959abb6e34
gull28 <krisjanisaugusts@gmail.com>
2024-04-27 15:14:21 +0300

    Added a wearability simulation

--- app/Models/WearSchedule.php
+++ app/Models/WearSchedule.php
@@ -18,6 +18,44 @@ class WearSchedule extends Model
     public function outfit(){
         return $this->belongsTo(Outfit::class);
     }
+
+    public function canAddToWearSchedule($outfitId, $date){
+        $outfit = Outfit::find($outfitId)->with('clothing')->get();
+        // get wear schedule for the outfit in the future
+        $wearSchedule = WearSchedule::where('outfit_id', $outfitId)->where('date', '>', now())->orderBy('date', 'asc')->get();
+
+        $clothing = $outfit->clothes;
+
+        $canWear = true;
+
+        // what this does is simulate the wear and was schedule for the outfit with simulated dates
+        // if the outfit has negative wear count at any point in time, it can't be added to the wear schedule
+        foreach($clothing as $c){
+            $clothingId = $clothing->id;
+            $wearCount = $clothing->wear_count;
+            $washCount = WashSchedule::where('clothing_id', $clothingId)->where('date', '>', now())->orderBy('date', 'asc')->get();
+
+            $schedule = $wearSchedule->merge($washSchedule)->push(['date' => $date])->sortBy('date');
+
+            foreach($schedule as $s){
+                if($s->clothing_id == $clothingId){
+                    if($s->type == 'wear'){
+                        $wearCount--;
+                    }else{
+                        $wearCount = $clothing->wear_count;
+                    }
+                    if($wearCount < 0){
+                        $canWear = false;
+                    }
+                }
+            }
+            if($wearCount < 0){
+                $canWear = false;
+            }
+        }
+        return $canWear;
+
+    }
     // public function canAddToWearSchedule($outfitId){
     //     $outfit = Outfit::find($outfitId)->with('clothing')->get();
     //     // get wear schedule for the outfit in the future
